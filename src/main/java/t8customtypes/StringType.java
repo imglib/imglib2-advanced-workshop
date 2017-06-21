df07742b7ffe77598f4a5dbce21c2a21ffcb517b
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-06-21 16:38:28 +0200

    formatting

--- src/main/java/t8customtypes/StringType.java
+++ src/main/java/t8customtypes/StringType.java
@@ -3,29 +3,28 @@ package t8customtypes;
 import net.imglib2.type.Type;
 
 /*
- * Your mission: 
- * 
- * - Complete this type class so that it can be used in a String image. You need 
- * to implement the Type interface; placeholders are already here.
- * 
- * - Try to guess what interfaces to implement to make an image based on this type
- * able to be used in a 
- *     - Maximum Intensity Projection (except that here we don't have an intensity
+ * Your mission:
+ *
+ * - Complete this type class so that it can be used in a String image. You need
+ *   to implement the Type interface; placeholders are already here.
+ *
+ * - Try to guess what interfaces to implement to make an image based on this
+ *   type able to be used in:
+ *   - Maximum Intensity Projection (except that here we don't have an intensity
  *     but strings.
- *     - A Summing Intensity Projection.
- *     
+ *   - Summing Intensity Projection.
+ *
  * - Then implement these interfaces here.
- * 
- * 
- * NOTE: 
- * This type is not relying on proxy objects like for instance the numeric types of ImgLib2.
- * Indeed, for these numeric types on real-world images it would be impractical to return  
- * an instance of the type for each pixel. The overhead would be gigantic. So FloatTye, 
- * UnsignedIntType, etc. are all based on proxy objects that allows for efficient image 
- * manipulation.
- * Here we don't have that: our type is not based on a proxy and there is one StringType object
- * per "pixel" in the image. So the number of string pixels we can have in an image with this
- * dummy solution has some practical limits.
+ *
+ *
+ * NOTE: This type is not relying on proxy objects like for instance the numeric
+ * types of ImgLib2. Indeed, for these numeric types on real-world images it
+ * would be impractical to return an instance of the type for each pixel. The
+ * overhead would be gigantic. So FloatTye, UnsignedIntType, etc. are all based
+ * on proxy objects that allows for efficient image manipulation. Here we don't
+ * have that: our type is not based on a proxy and there is one StringType
+ * object per "pixel" in the image. So the number of string pixels we can have
+ * in an image with this dummy solution has some practical limits.
  */
 
 /**
@@ -33,9 +32,8 @@ import net.imglib2.type.Type;
  */
 public class StringType implements Type< StringType >
 {
-
 	@Override
-	public boolean valueEquals( StringType t )
+	public boolean valueEquals( final StringType t )
 	{
 		// TODO Auto-generated method stub
 		return false;
@@ -56,18 +54,25 @@ public class StringType implements Type< StringType >
 	}
 
 	@Override
-	public void set( StringType t )
+	public void set( final StringType t )
 	{
 		// TODO Auto-generated method stub
 	}
 
+	@Override
+	public String toString()
+	{
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
 	/**
 	 * An extra method to modify the content of this type from a string.
-	 * 
+	 *
 	 * @param t
 	 *            the string to wrap in this type.
 	 */
-	public void set( String t )
+	public void set( final String t )
 	{
 		// TODO Auto-generated method stub
 	}
